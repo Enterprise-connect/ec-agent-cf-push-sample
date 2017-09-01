a304a87e5a2ee7be8cbb95ab2d51819b8413be08
phiwoff <philip.wofford@ge.com>
2017-09-01 09:29:12 -0400

    Updated the example ec.sh
    I believe with <bracketed placeholders> users will be able to understand this example more clearly without having to reference the EC Agent flag reference documentation.

--- ec.sh
+++ ec.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-#./ecagent_linux -mod gateway -lpt ${PORT} -zon 156453ff-07e3-4bbc-9d95-6b3482987784 -sst https://156453ff-07e3-4bbc-9d95-6b3482987784.run.asv-pr.ice.predix.io -dbg -tkn YWRtaW46OUVEMTkxdEBJMTU2NDUzZmYtMDdlMy00YmJjLTlkOTUtNmIzNDgyOTg3Nzg0
-#./ecagent_linux -aid 191001 -hst wss://ec-chia-gateway-1.run.aws-usw02-pr.ice.predix.io/agent -rht 10.72.6.143 -rpt 5432 -cid chia -csc chia -oa2 https://67c431e9-29e8-49e5-95b2-105660686261.predix-uaa.run.aws-usw02-pr.ice.predix.io/oauth/token -dur 300 -mod server -hca ${PORT} -dbg
-./ecagent_linux -mod server -aid 191001 -hst wss://ec-test-gateway-3.run.asv-pr.ice.predix.io/agent -rht postgres-service-pr-gslb.gecis.io -rpt 5432 -cid yasuda -csc yasuda -oa2 https://a04a0b99-2a09-4df1-afe2-f045e4b1649d.predix-uaa.run.asv-pr.ice.predix.io/oauth/token -dur 300 -dbg -hca ${PORT}
+#./ecagent_linux_sys -mod gateway -lpt ${PORT} -zon <Predix-Zone-ID> -sst <EC-Service-URI> -tkn <admin-token> -dbgx
+./ecagent_linux_sys -mod server -aid <VCAP_provided> -cid <UAA_client_ID> -csc <UAA_client_Secret> -dur 10000 -hst wss://<Predix_Gateway_App_URL>/agent -oa2 https://<predixUAA_URL>/oauth/token -rht 10.11.12.13 -rpt 5432 -dbg -hca ${PORT}
