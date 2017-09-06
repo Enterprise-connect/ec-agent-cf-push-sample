c66b0913959fa0e23f9862c2b6b9f384bd510585
phiwoff <philip.wofford@ge.com>
2017-09-06 11:25:11 -0400

    Update ec.sh

--- ec.sh
+++ ec.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-#./ecagent_linux_sys -mod gateway -lpt ${PORT} -zon <Predix-Zone-ID> -sst <EC-Service-URI> -tkn <admin-token> -dbgx
-./ecagent_linux_sys -mod server -aid <VCAP_provided> -cid <UAA_client_ID> -csc <UAA_client_Secret> -dur 10000 -hst wss://<Predix_Gateway_App_URL>/agent -oa2 https://<predixUAA_URL>/oauth/token -rht 10.11.12.13 -rpt 5432 -dbg -hca ${PORT}
+#./ecagent_linux_sys -mod gateway -lpt ${PORT} -zon <Predix-Zone-ID> -sst <EC-Service-URI> -tkn <admin-token> -dbg
+
+#./ecagent_linux_sys -mod server -aid <VCAP_provided, "id"> -cid <UAA_client_ID> -csc <UAA_client_Secret> -dur 10000 -hst wss://<Predix_Gateway_App_URL>/agent -oa2 https://<predixUAA_URL>/oauth/token -rht 10.11.12.13 -rpt 5432 -dbg -hca ${PORT}
+
+#./ecagent_linux_sys -mod client -aid <Cannot be the same as Server -aid> -cid <UAA_client_ID> -csc <UAA_client_Secret> -dur 3000 -hst wss://<Predix_Gateway_App_URL>/agent -tid <EC Server -aid> -oa2 https://<predixUAA_URL>/oauth/token -lpt <Defined_by_You> -dbg
