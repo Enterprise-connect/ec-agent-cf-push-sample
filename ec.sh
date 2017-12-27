0a76c38c26f2859f9549aef13263a3b983202585
phiwoff <philip.wofford@ge.com>
2017-12-27 10:33:57 -0500

    enhanced meaningful separation of flags
    While trying to get the 'word wrap' right in the non-technical docs, I realized I could apply the same logic to the example here to hopefully make the relevance of the various flags more meaningful to users.

--- ec.sh
+++ ec.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+# Gateway
+#./ecagent_linux_sys -mod gateway -lpt ${PORT} \
+#-zon <Predix-Zone-ID> -sst <EC-Service-URI> \
+#-tkn <admin-token> \
+#-dbg
 
-#./ecagent_linux_sys -mod gateway -lpt ${PORT} -zon <Predix-Zone-ID> -sst <EC-Service-URI> -tkn <admin-token> -dbg
+# Server
+#./ecagent_linux_sys -mod server -aid <VCAP_provided, "id"> \
+#-cid <UAA_client_ID> -csc <UAA_client_Secret> -dur 3000 \
+#-oa2 https://<predixUAA_URL>/oauth/token \
+#-hst wss://<Predix_Gateway_App_URL>/agent \
+#-rht 10.11.12.13 -rpt 5432 \
+#-dbg -hca ${PORT}
 
-#./ecagent_linux_sys -mod server -aid <VCAP_provided, "id"> -cid <UAA_client_ID> -csc <UAA_client_Secret> -dur 3000 -hst wss://<Predix_Gateway_App_URL>/agent -oa2 https://<predixUAA_URL>/oauth/token -rht 10.11.12.13 -rpt 5432 -dbg -hca ${PORT}
-
-#./ecagent_linux_sys -mod client -aid <Cannot be the same as Server -aid> -tid <EC Server -aid> -cid <UAA_client_ID> -csc <UAA_client_Secret> -dur 3000 -hst wss://<Predix_Gateway_App_URL>/agent -oa2 https://<predixUAA_URL>/oauth/token -lpt <Defined_by_You> -dbg
+# Client
+#./ecagent_linux_sys -mod client \
+#-aid <Cannot be the same as Server -aid> -tid <EC Server -aid> \
+#-cid <UAA_client_ID> -csc <UAA_client_Secret> -dur 3000 \
+#-oa2 https://<predixUAA_URL>/oauth/token \
+#-hst wss://<Predix_Gateway_App_URL>/agent \
+#-lpt <Defined_by_You> \
+#-dbg
